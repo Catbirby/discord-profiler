a59a65610292ee595657a3cc315f308f1417a8e6
Elliston James <ellistonjames9@gmail.com>
2017-12-04 22:19:22 -0800

    Base files

---
+++ main.js
@@ -0,0 +1,28 @@
+const DiscordRPC = require('discord-rpc');
+const profile = require('./profile.json')
+const ClientId = '387458498771812363';
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+  if (!rpc)
+    return;
+
+  rpc.setActivity({
+    details: profile.title,
+    state: profile.quote,
+	largeImageKey: profile.mediaLarge,
+    largeImageText: profile.userLarge,
+    smallImageKey: profile.mediaSmall,
+    smallImageText: profile.userSmall,
+    instance: false,
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+  console.log("ready");
+});
+
+rpc.login(ClientId).catch(console.error);
